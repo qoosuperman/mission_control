601c727de7bb80672acfecf2bd005601eef7361e
Anthony Chao <qoosuperman@gmail.com>
2019-10-18 22:15:13 +0800

    add new/create function

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -2,4 +2,25 @@ class MissionsController < ApplicationController
   def index
     @missions = Mission.all
   end
+
+  def new
+    @mission = Mission.new
+  end
+
+  def create
+    @mission = Mission.new(mission_params)
+    #因為還沒建後台，先建一個 user 把 mission 都丟給他
+    @mission.user = User.first
+    if @mission.save
+      redirect_to root_path, notice: "新增任務成功！"
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def mission_params
+    params.require(:mission).permit(:title, :priority, :category)
+  end
 end
